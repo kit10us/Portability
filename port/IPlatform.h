d9bebb246975affa82606d1c8325a7398a459853
Stacy Smith <stacy@kit10.us>
2024-06-28 14:03:04 -0700

    Added licensing.

--- port/IPlatform.h
+++ port/IPlatform.h
@@ -1,22 +1,22 @@
 /*
- * Unify Library
- * https://github.com/kit10us/unify
- * Copyright (c) 2002, Kit10 Studios LLC
+ * Portability Library
+ * https://github.com/kit10us/Portability
+ * Copyright (c) 2024, Kit10 Studios LLC
  *
- * This file is part of Unify Library (a.k.a. Unify)
+ * This file is part of Portability Library (a.k.a. Port)
  *
- * Unify is free software: you can redistribute it and/or modify
+ * Port is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Unify is distributed in the hope that it will be useful,
+ * Port is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Unify.  If not, see <https://www.gnu.org/licenses/>.
+ * along with Port.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #pragma once
